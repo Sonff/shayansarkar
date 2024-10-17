74ce73de13afc153b50305979310a811034b8f26
sonff <sroy52649@gmail.com>
2024-10-17 10:43:05 +0530

    first commit

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-298.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-41d1251b', {"layout":"bars","heading":"WORDPRESS","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":85,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-4c65bf33', {"layout":"bars","heading":"PHP","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":95,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-4937ccd4', {"layout":"bars","heading":"HOSTINGER","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":75,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-edaa0655', {"layout":"bars","heading":"WEB DEVELOPEMENT","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":80,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
